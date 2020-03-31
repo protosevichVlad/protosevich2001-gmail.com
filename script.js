1677c6cd9378346674e8bc547407a81a288c0cfd
Protosevich <protosevich2001@gmail.com>
2020-03-31 13:30:17 +0300

    feat: add buttons

--- script.js
+++ script.js
@@ -4,26 +4,48 @@ class Keyboard{
   static lang = 'en';
   static case = 'lowerCase';
   static textarea;
+  static keyboard_element;
   buttons = [];
 
   constructor(){
     //creating textarea
+    let wrapper = document.createElement('div')
+    wrapper.classList.add('wrapper')
+    document.body.appendChild(wrapper)
+
     Keyboard.textarea = document.createElement('textarea');
     Keyboard.textarea.id = 'textarea';
     Keyboard.textarea.setAttribute('readonly', true);
-    document.body.appendChild(Keyboard.textarea);
+    wrapper.appendChild(Keyboard.textarea);
     Keyboard.textarea.selectionStart = 0;
+  
+    Keyboard.keyboard_element = document.createElement('div')
+    Keyboard.keyboard_element.classList.add('keyboard')
+    Keyboard.keyboard_element.id = 'keyboard'
+    wrapper.appendChild(Keyboard.keyboard_element)
 
+    
+    let br = document.createElement('br');
 
     //add buttons
-    this.buttons.push(new Button('Digit1', 49, {
-      'en':{
-        'upperCase': '!',
-        'lowerCase': '1',
+    this.buttons.push(new Button('Backquote', 192, {
+      en:{
+        upperCase: '~',
+        lowerCase: '`',
       },
-      'ru':{
-        'upperCase': '!',
-        'lowerCase': '1',
+      ru:{
+        upperCase: 'Ё',
+        lowerCase: 'ё',
+      },
+    }));
+    this.buttons.push(new Button('Digit1', 49, {
+      en:{
+        upperCase: '!',
+        lowerCase: '1',
+      },
+      ru:{
+        upperCase: '!',
+        lowerCase: '1',
       },
     }));
     this.buttons.push(new Button('Digit2', 50, {
@@ -36,6 +58,127 @@ class Keyboard{
         lowerCase: '2',
       },
     }));
+    this.buttons.push(new Button('Digit3', 51, {
+      en:{
+        upperCase: '#',
+        lowerCase: '3',
+      },
+      ru:{
+        upperCase: '№',
+        lowerCase: '3',
+      },
+    }));
+    this.buttons.push(new Button('Digit4', 52, {
+      en:{
+        upperCase: '$',
+        lowerCase: '4',
+      },
+      ru:{
+        upperCase: ';',
+        lowerCase: '4',
+      },
+    }));
+    this.buttons.push(new Button('Digit5', 53, {
+      en:{
+        upperCase: '%',
+        lowerCase: '5',
+      },
+      ru:{
+        upperCase: '%',
+        lowerCase: '5',
+      },
+    }));
+    this.buttons.push(new Button('Digit6', 54, {
+      en:{
+        upperCase: '^',
+        lowerCase: '6',
+      },
+      ru:{
+        upperCase: ':',
+        lowerCase: '6',
+      },
+    }));
+    this.buttons.push(new Button('Digit7', 55, {
+      en:{
+        upperCase: '&',
+        lowerCase: '7',
+      },
+      ru:{
+        upperCase: '?',
+        lowerCase: '7',
+      },
+    }));
+    this.buttons.push(new Button('Digit8', 56, {
+      en:{
+        upperCase: '*',
+        lowerCase: '8',
+      },
+      ru:{
+        upperCase: '*',
+        lowerCase: '8',
+      },
+    }));
+    this.buttons.push(new Button('Digit9', 57, {
+      en:{
+        upperCase: '(',
+        lowerCase: '9',
+      },
+      ru:{
+        upperCase: '(',
+        lowerCase: '9',
+      },
+    }));
+    this.buttons.push(new Button('Digit0', 48, {
+      en:{
+        upperCase: ')',
+        lowerCase: '0',
+      },
+      ru:{
+        upperCase: ')',
+        lowerCase: '0',
+      },
+    }));
+    this.buttons.push(new Button('Minus', 189, {
+      en:{
+        upperCase: '_',
+        lowerCase: '-',
+      },
+      ru:{
+        upperCase: '_',
+        lowerCase: '-',
+      },
+    }));
+    this.buttons.push(new Button('Equal', 187, {
+      en:{
+        upperCase: '+',
+        lowerCase: '=',
+      },
+      ru:{
+        upperCase: '+',
+        lowerCase: '=',
+      },
+    }));
+    this.buttons.push(new Button('Backspace', 8, {
+      en:{
+        upperCase: 'Backspace',
+        lowerCase: 'Backspace',
+      },
+      ru:{
+        upperCase: 'Backspace',
+        lowerCase: 'Backspace',
+      },
+    }, 'special', 'backspace'));
+    Keyboard.keyboard_element.appendChild(br);
+    this.buttons.push(new Button('Tab', 9, {
+      en:{
+        upperCase: 'Tab',
+        lowerCase: 'Tab',
+      },
+      ru:{
+        upperCase: 'Tab',
+        lowerCase: 'Tab',
+      },
+    }, 'special', 'backspace'));
 
     window.addEventListener('keydown', this.onKeyDown.bind(this));
     window.addEventListener('keyup', this.onKeyUp.bind(this));
@@ -60,7 +203,7 @@ class Keyboard{
 }
 
 class Button{
-  constructor(code, key_code, dict){
+  constructor(code, key_code, dict, ...class_button){
     this.dict = dict;
     this.key_code = key_code
     this.code = code;
@@ -75,9 +218,10 @@ class Button{
     this.element_in_html.addEventListener('mouseenter', this.onMouseDownEnter.bind(this));
 
     this.element_in_html.classList = ["button"];
-    document.body.appendChild(this.element_in_html);
+    if (class_button !== undefined) this.element_in_html.classList.add(...class_button)
+    Keyboard.keyboard_element.appendChild(this.element_in_html);
 
-    }
+  }
 
   
   onMouseUp(event){
@@ -102,21 +246,24 @@ class Button{
     let cursor_position = textarea.selectionStart;
     let value = textarea.value;
 
-    console.log('in', cursor_position, value.length);
-    
+    if(this.dict[Keyboard.lang][Keyboard.case].length == 1){
+      if(value.length > cursor_position){
+        textarea.value = value.slice(0, cursor_position) + this.dict[Keyboard.lang][Keyboard.case] + value.slice(cursor_position, value.length);
+        textarea.selectionStart = cursor_position + 1;
+        textarea.selectionEnd = cursor_position + 1;
+      }
+      else{
+        textarea.value += this.dict[Keyboard.lang][Keyboard.case];
+      }
+    }else{
+      if(this.dict[Keyboard.lang][Keyboard.case] == 'Backspace'){
+        textarea.value = value.slice(0, textarea.selectionStart) + value.slice(textarea.selectionEnd, value.length);
+        textarea.selectionStart = cursor_position - 1;
+        textarea.selectionEnd = cursor_position - 1;
+      }else if(this.dict[Keyboard.lang][Keyboard.case] == 'Backspace'){
 
-    if(value.length > cursor_position){
-      textarea.value = value.slice(0, cursor_position) + this.dict[Keyboard.lang][Keyboard.case] + value.slice(cursor_position, value.length);
-      textarea.selectionStart = cursor_position + 1;
-      textarea.selectionEnd = cursor_position + 1;
+      }
     }
-    else{
-      textarea.value += this.dict[Keyboard.lang][Keyboard.case];
-    }
-
-    console.log('out', textarea.selectionStart, textarea.value.length);
-
-    
   }
 }
 
